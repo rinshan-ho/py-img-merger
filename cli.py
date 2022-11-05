d62f87c2728b85627247207df8d0638055a49c30
menfooon <lazuli.amaryllis@gmail.com>
2022-11-06 02:32:06 +0900

    python用 論理演算子の修正

--- cli.py
+++ cli.py
@@ -4,10 +4,10 @@ import sys
 import configparser
 
 dir = os.path.dirname(sys.argv[0])
-config_path = dir + '\\config.ini'
+# config_path = dir + '\\config.ini'
 config_ini = configparser.ConfigParser()
-config_ini.read(config_path, encoding = 'utf-8')
-# config_ini.read('config.ini', encoding = 'utf-8')
+# config_ini.read(config_path, encoding = 'utf-8')
+config_ini.read('config.ini', encoding = 'utf-8')
 
 DEFAULT_NAME = config_ini.get('DEFAULT', 'name')
 DEFAULT_EXT = config_ini.get('DEFAULT', 'ext')
@@ -46,16 +46,18 @@ for path in img_paths:
 #大小関係なしにAtoZソート
 imgs.sort(key=str.casefold)
 
+print(f'出力先パス: {OUTPUT_PATH}\n')
+
 #画像が存在するとき
 if(imgs!=[]):
     #imgsをimreadし再格納
     for i, img in enumerate(imgs):
         imgs[i] = cv2.imread(img)
 
-    name = input('結合した画像の名前を入力してください\n(デフォルトは"merged"です)\n>> ')
-    ext = input('拡張子を入力してください\n(デフォルトは".png"です)\n>> .')
-    method = input('結合方式を選択してください(縦:0, 横:1)\n(デフォルトは"1"です)\n>> ')
-    scaling = input('結合基準を選択してください(最小px:0, 最大px:1)\n(デフォルトは"0"です)\n>> ')
+    name = input(f'結合した画像の名前を入力してください\n(デフォルトは"{DEFAULT_NAME}"です)\n>> ')
+    ext = input(f'拡張子を入力してください\n(デフォルトは".{DEFAULT_EXT}"です)\n>> .')
+    method = input(f'結合方式を選択してください(横:0, 縦:1)\n(デフォルトは"{DEFAULT_METHOD}"です)\n>> ')
+    scaling = input(f'結合基準を選択してください(最小px:0, 最大px:1)\n(デフォルトは"{DEFAULT_SCALING}"です)\n>> ')
 
 
     #DEFAULT値の設定
@@ -66,14 +68,16 @@ if(imgs!=[]):
     else:
         #先頭に”.”が付いていたら除去
         ext = ext.removeprefix('.')
-    if(method=='' or method!='0' or method!='1'):
+    if(method=='' or (method!='0' and method!='1')):
         method = DEFAULT_METHOD
-    if(scaling=='' or scaling!='0' or scaling!='1'):
+    if(scaling=='' or (scaling!='0' and scaling!='1')):
         scaling = DEFAULT_SCALING
 
 
     if(method=='0'):
         img = hconcat(imgs, scaling)
+    if(method=='1'):
+        img = vconcat(imgs, scaling)
     
     if(method=='1'):
         img = vconcat(imgs, scaling)
