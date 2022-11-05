661f28f8656705dca0909d5829ef8782dcbcc690
menfooon <lazuli.amaryllis@gmail.com>
2022-11-06 00:42:16 +0900

    最大pxに合わせて結合することを出来るようにした

--- cli.py
+++ cli.py
@@ -6,23 +6,32 @@ import configparser
 config_ini = configparser.ConfigParser()
 config_ini.read('config.ini', encoding = 'utf-8')
 
-DEFAULT_NAME = config_ini.get('DEFAULT','name')
-DEFAULT_EXT = config_ini.get('DEFAULT','ext')
-DEFAULT_METHOD = config_ini.get('DEFAULT','method')
-OUTPUT_PATH = config_ini.get('DEFAULT','path')
+DEFAULT_NAME = config_ini.get('DEFAULT', 'name')
+DEFAULT_EXT = config_ini.get('DEFAULT', 'ext')
+DEFAULT_METHOD = config_ini.get('DEFAULT', 'method')
+DEFAULT_SCALING = config_ini.get('DEFAULT' ,'scaling')
+OUTPUT_PATH = config_ini.get('DEFAULT', 'path')
 
-def hconcat(img_list):
-    #最も小さい高さを取得
-    min_height = min(img.shape[0] for img in img_list)
-    #最も小さい高さに合わせてサイズの変更
-    img_list_resize = [cv2.resize(img, (int(img.shape[1] * min_height / img.shape[0]), min_height)) for img in img_list]
+def hconcat(img_list, scaling):
+    if(scaling=='0'):
+        #最小pxを取得
+        height = min(img.shape[0] for img in img_list)
+    elif(scaling=='1'):
+        #最大pxを取得
+        height = max(img.shape[0] for img in img_list)
+    #取得したサイズに拡大縮小
+    img_list_resize = [cv2.resize(img, (int(img.shape[1] * height / img.shape[0]), height)) for img in img_list]
     return cv2.hconcat(img_list_resize)
 
-def vconcat(img_list):
-    #最も小さい幅を取得
-    min_width = min(img.shape[1] for img in img_list)
-    #最も小さい幅に合わせてサイズの変更
-    img_list_resize = [cv2.resize(img, (min_width, int(img.shape[0] * min_width / img.shape[1]))) for img in img_list]
+def vconcat(img_list, scaling):
+    if(scaling=='0'):
+        #最小pxを取得
+        width = min(img.shape[1] for img in img_list)
+    elif(scaling=='1'):
+        #最大pxを取得
+        width = max(img.shape[1] for img in img_list)
+    #取得したサイズに拡大縮小
+    img_list_resize = [cv2.resize(img, (width, int(img.shape[0] * width / img.shape[1]))) for img in img_list]
     return cv2.vconcat(img_list_resize)
 
 
@@ -43,6 +52,8 @@ if(imgs!=[]):
     name = input('結合した画像の名前を入力してください\n(デフォルトは"merged"です)\n>> ')
     ext = input('拡張子を入力してください\n(デフォルトは".png"です)\n>> .')
     method = input('結合方法を選択してください(縦:0, 横:1)\n(デフォルトは"0"です)\n>> ')
+    scaling = input('結合基準を選択してください(最小px:0, 最大px:1)\n(デフォルトは"0"です)\n>> ')
+
 
     #DEFAULT値の設定
     if(name==''):
@@ -52,14 +63,17 @@ if(imgs!=[]):
     else:
         #先頭に”.”が付いていたら除去
         ext = ext.removeprefix('.')
-    if(method==''):
+    if(method=='' or method!='0' or method!='1'):
         method = DEFAULT_METHOD
+    if(scaling=='' or scaling!='0' or scaling!='1'):
+        scaling = DEFAULT_SCALING
 
 
     if(method=='0'):
-        img = hconcat(imgs)
-    elif(method=='1'):
-        img = vconcat(imgs)
+        img = hconcat(imgs, scaling)
+    
+    if(method=='1'):
+        img = vconcat(imgs, scaling)
 
     #OUTPUT_PATHに画像を出力
     if not os.path.exists(OUTPUT_PATH):
