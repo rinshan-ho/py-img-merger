7634b7701ac44adbac2af06e150930369cdf1229
rinshan_ho <lazuli.amaryllis@gmail.com>
2023-06-30 15:04:30 +0900

    requirements.txtの追加, run.batの削除

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import cv2
 import sys
 import configparser
 
-dir = os.path.dirname(sys.argv[0])
-# config_path = dir + '\\config.ini'
 config_ini = configparser.ConfigParser()
 # config_ini.read(config_path, encoding = 'utf-8')
 config_ini.read('config.ini', encoding = 'utf-8')
@@ -14,6 +12,7 @@ DEFAULT_EXT = config_ini.get('DEFAULT', 'ext')
 DEFAULT_METHOD = config_ini.get('DEFAULT', 'method')
 DEFAULT_SCALING = config_ini.get('DEFAULT' ,'scaling')
 OUTPUT_PATH = config_ini.get('DEFAULT', 'path')
+print(OUTPUT_PATH)
 
 def hconcat(img_list, scaling):
     if(scaling=='0'):
@@ -38,40 +37,31 @@ def vconcat(img_list, scaling):
     return cv2.vconcat(img_list_resize)
 
 
-#画像の取得
-imgs = []
-img_paths = sys.argv[1:]
-for path in img_paths:
-    imgs.append(path)
+#D&Dされた画像の取得
+imgs = sys.argv[1:]
 #大小関係なしにAtoZソート
 imgs.sort(key=str.casefold)
 
 print(f'出力先パス: {OUTPUT_PATH}\n')
 
-#画像が存在するとき
+#画像がD&Dされた場合
 if(imgs!=[]):
     #imgsをimreadし再格納
     for i, img in enumerate(imgs):
         imgs[i] = cv2.imread(img)
 
-    name = input(f'連結した画像の名前を入力してください\n(デフォルトは"{DEFAULT_NAME}"です)\n>> ')
+    name = input(f'連結後の画像の名前を入力してください\n(デフォルトは"{DEFAULT_NAME}"です)\n>> ')
     ext = input(f'拡張子を入力してください\n(デフォルトは".{DEFAULT_EXT}"です)\n>> .')
     method = input(f'連結方式を選択してください(横:0, 縦:1)\n(デフォルトは"{DEFAULT_METHOD}"です)\n>> ')
     scaling = input(f'連結基準を選択してください(最小px:0, 最大px:1)\n(デフォルトは"{DEFAULT_SCALING}"です)\n>> ')
 
 
     #DEFAULT値の設定
-    if(name==''):
-        name = DEFAULT_NAME
-    if(ext==''):
-        ext = DEFAULT_EXT
-    else:
-        #先頭に”.”が付いていたら除去
-        ext = ext.removeprefix('.')
-    if(method=='' or (method!='0' and method!='1')):
-        method = DEFAULT_METHOD
-    if(scaling=='' or (scaling!='0' and scaling!='1')):
-        scaling = DEFAULT_SCALING
+    if(name==''): name = DEFAULT_NAME
+    ext = ext.removeprefix('.') #先頭に”.”が付いていたら除去
+    if(ext==''): ext = DEFAULT_EXT
+    if(method=='' or (method!='0' and method!='1')): method = DEFAULT_METHOD
+    if(scaling=='' or (scaling!='0' and scaling!='1')): scaling = DEFAULT_SCALING
 
 
     if(method=='0'):
@@ -87,5 +77,8 @@ if(imgs!=[]):
         os.makedirs(OUTPUT_PATH)
     cv2.imwrite(f'{OUTPUT_PATH}\\{name}.{ext}', img)
     print(f'{OUTPUT_PATH}\\{name}.{ext}')
+
+# D&Dされてないとき
 else:
     print("画像をD&Dしてください\n")
+    input("終了する場合はなにかキーを入力してください")
